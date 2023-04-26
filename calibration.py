378dbbe1f04607928138fccebd8f57d3f6a4997b
hsu1261 <xuzing105525@gmail.com>
2023-04-26 13:34:28 +0800

    Ignore calibration image

--- calibration.py
+++ calibration.py
@@ -86,9 +86,9 @@ if __name__ == '__main__':
     # Process===========================================================================================
     if not CALIBRATION:
         for i in img_list:
-            if 'hsi' in i:
+            if 'hsi' in i and i != "hsi_cali.png":
                 hsi_dict[i.split(".")[0]] = cv2.imread(i, cv2.IMREAD_COLOR)
-            elif 'wl' in i:
+            elif 'wl' in i and i != "wl_cali.png":
                 wl_dict[i.split(".")[0]] = cv2.imread(i, cv2.IMREAD_COLOR)
 
         if (not hsi_dict) or (not wl_dict):
